b3f30d911300ffb3c2df8da35b21edee64f85441
smj <smj123432@gmail.com>
2026-03-09 10:00:34 +0900

    리액트 데이터바인딩

---
+++ learing-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -0,0 +1,11 @@
+import S from "./style.module.css";
+import Footer from "@/components/Footer";
+
+export default function App() {
+  return (
+    <div className={S.container}>
+      <h1>React App</h1>
+      <Footer />
+    </div>
+  );
+}
---
+++ learing-react/02-State-이벤트-핸들링/src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import S from "./style.module.css";
+
+export default function Footer() {
+  /**
+   * 데이터 바인딩 (Data Binding = Data + JSX = Markup)
+   * - JavaScript 데이터(변수, 상수, 계산된 값 등)
+   * - JSX 구문 안에 데이터 끼워넣기(Interpolation, {data} )
+   */
+  const currentYear = new Date().getFullYear();
+
+  /**
+   * Props로 데이터 전달(부모 → 자식)
+   * - 부모에서 자식으로 전달하는 데이터
+   * - React.createElement API에서 Props
+   * - Props 타입 정의 (인라인 → 인터페이스)
+   */
+  const slogan = "완벽보다 완주를!";
+
+  /**
+   * 접근성 고려
+   * - 사용자가 이해하기 쉽고 읽기 쉽도록 저작권 레이블(label) 구성
+   */
+  const copyrightLabel = `모든 저작권은 이듬(EUID)에 있습니다. ${slogan}`;
+
+  return (
+    <footer className={S.footer}>
+      <small aria-label={copyrightLabel}>
+        {currentYear} COPYRIGHT RESERVED. © <abbr title="이듬(EUID)">EUID</abbr>
+        . {slogan}
+      </small>
+    </footer>
+  );
+}
