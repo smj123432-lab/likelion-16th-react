d1c98fdadfd1b7c7337b33a25d530d7e7c690340
smj <smj123432@gmail.com>
2026-03-12 14:10:47 +0900

    learn 리스트 렌더링과 유니크한 키의 중요성

--- learing-react/04-리스트-렌더링-설계/src/app/index.tsx
+++ learing-react/04-리스트-렌더링-설계/src/app/index.tsx
@@ -1,11 +1,11 @@
 // import ProductPage from "@/practices/02-filtering/pages/product";
 import S from "./style.module.css";
-import { StaffList } from "@/components";
+import { ShiftManager } from "@/components";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <StaffList />
+      <ShiftManager />
     </div>
   );
 }
